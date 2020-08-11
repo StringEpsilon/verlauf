4e4c3e59a6ffa91b98d1849d983211eb496f8326
StringEpsilon <StringEpsilon@gmail.com>
2020-08-11 13:30:11 +0200

    Make resulting package a little flatter and add more entry files.

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,6 +2,28 @@ import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
 import typescript from "rollup-plugin-typescript2";
 import dts from "rollup-plugin-dts";
+import copy from "rollup-plugin-copy";
+
+function makeEsmConfig(input, output) {
+	return {
+		input: input,
+		output: [
+			{
+				file: output,
+				format: "esm",
+				plugins: [terser({ numWorkers: 1 })],
+			},
+		],
+		plugins: [
+			typescript({
+				clean: true,
+				tsconfigOverride: {
+					compilerOptions: { target: "ES6", module: "ESNext" },
+				},
+			}),
+		],
+	};
+}
 
 const config = [
 	{
@@ -25,28 +47,23 @@ const config = [
 			}),
 		],
 	},
-	{
-		input: "src/index.ts",
-		output: [
-			{
-				file: `dist/esm/${pkg.name}.js`,
-				format: "esm",
-				plugins: [terser({ numWorkers: 1 })],
-			},
-		],
-		plugins: [
-			typescript({
-				clean: true,
-				tsconfigOverride: {
-					compilerOptions: { target: "ES6", module: "ESNext" },
-				},
-			}),
-		],
-	},
+	makeEsmConfig("src/adapters/browserHistory.ts", "dist/createBrowserHistory.js"),
+	makeEsmConfig("src/adapters/hashHistory.ts", "dist/createHashHistory.js"),
+	makeEsmConfig("src/adapters/memoryHistory.ts", "dist/createMemoryHistory.js"),
+	makeEsmConfig("src/index.ts", `dist/esm/${pkg.name}.js`,),
 	{
 		input: "./src/index.ts",
 		output: [{ file: "dist/verlauf.d.ts", format: "es" }],
-		plugins: [dts()],
+		plugins: [
+			dts(),
+			copy({
+				targets: [
+					{ src: 'package.json', dest: 'dist/' },
+					{ src: 'README.md', dest: 'dist/' },
+				]
+			})
+		],
 	},
 ];
+
 export default config;
