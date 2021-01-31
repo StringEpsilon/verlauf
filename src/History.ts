143cd2bb1d1c262a79953453dd19df8ad7d7abfc
StringEpsilon <StringEpsilon@gmail.com>
2021-01-31 15:15:59 +0100

    Restructured history class to reduce output size.
    Saves ~300 bytes.

--- src/History.ts
+++ src/History.ts
@@ -26,61 +26,171 @@ const defaultUserConfirmation: getUserConfirmation = (message, callback) => {
 /**
  * History object, providing all APIs to interact with browser, hash or memory history.
  */
-export class History {
-	private _listeners: NavigationListener[] = [];
-	private _historyAdapter: HistoryAdapter;
-	private _options: HistoryOptions;
-	private _blocker: TransitionBlocker;
-	private _pendingTransition: boolean = false;
-
+export declare interface History {
 	/** Current length of the history. Value is retrived from historyAdapter.getLength() after each action (push, replace, pop). */
-	public length: number;
+	length: number;
 	/** Current location. Value is retrieved from historyAdapter.getLocation() after each action (push, replace, pop). */
-	public location: Location;
+	location: Location;
 	/** Last action performen on the history stack. Initial value is always POP. */
-	public action: string = ACTION.POP;
+	action: string;
 
+	/** @ignore */
+	_adapterCallback: (newLocation: Location) => void;
+	/** @ignore */
+	_alertListeners: (action: string) => void;
+	/** @ignore */
+	_transition: (
+		target: Location,
+		action: string,
+		onSuccess: Function,
+		onFailure?: Function
+	) => void;
 	/**
-	 * Creates a new History instance.
-	 *
-	 * @param createAdapter The function to create a {@link HistoryAdapter} instance.
-	 * @param options The options for the History instance and the HistoryAdapter.
+	 * Register a listener for all changes in location.
+	 * @param listener The callback to register. Will be called with {@link Location} and action.
+	 * @returns A callback to unregister the newly added listener.
 	 */
-	constructor(
-		createAdapter: (
-			listner: OnAdapterLocationChange,
-			options: HistoryOptions
-		) => HistoryAdapter,
-		options?: HistoryOptions
-	) {
-		this._options = options || {};
-		if (!this._options.basename) {
-			this._options.basename = "";
-		}
-		if (!this._options.keyLength) {
-			this._options.keyLength = 6;
-		}
-		if (!this._options.getUserConfirmation) {
-			this._options.getUserConfirmation = defaultUserConfirmation;
-		}
-		if (!this._options.createBlocker) {
-			this._options.createBlocker = LegacyBlocker;
-		}
+	listen: (listener: NavigationListener) => () => void;
+	/**
+	 * Removes / disables a listener.
+	 *
+	 * @param listener The listener to disable.
+	 */
+	unlisten: (listener: NavigationListener) => void;
+	/**
+	 * Creates an appropriate href target string for a given location.
+	 * @param target
+	 */
+	createHref: (target: Location) => string;
+	/**
+	 * Navigate to a location with the specified method.
+	 *
+	 * @param target Target location.
+	 * @param state Desired state for the location
+	 * @param method Specify the method to use for navigation. Either "PUSH" or "REPLACE".
+	 * Default: PUSH.
+	 */
+	navigate: (
+		target: string | Location,
+		state: object | null,
+		method?: string
+	) => void;
+	/**
+	 * Push a new location to the history stack and navigate to it.
+	 *
+	 * @param target Location to go to. Either a pathname or a complete location object.
+	 * @param state Optional state to push with the location.
+	 */
+	push: (target: string | Location, state?: object | null) => void;
+	/**
+	 * Replace the current location and state on the stack.
+	 *
+	 * @param target Location or pathname to navigate to.
+	 * @param state Desired state
+	 */
+	replace: (target: string | Location, state?: object | null) => void;
+	/**
+	 * Configure a transition block. {@link LegacyBlocker}
+	 *
+	 * @param args - Arguments passed to {@link LegacyBlocker.block}
+	 * @returns A callback to remove the block.
+	 */
+	block: (...args: any[]) => () => void;
+	/**
+	 * Remove the currently configured blocker.
+	 */
+	unblock: () => void;
+	/**
+	 * Shorthand for go(1). Go forward in history by one entry.
+	 */
+	goBack: () => void;
+	/**
+	 * Shorthand for go(-1). Go forward in history by one entry.
+	 */
+	goForward: () => void;
+	/**
+	 * Goes forwards or backwards in history. If the value is out of bounds, it will go as far as it can (i.E. to the first or last entry).
+	 *
+	 * @param steps Number of steps to go forward / backward.
+	 * @example
+	 * ```js
+	 * history.go(-10); // go ten entries back.
+	 *
+	 * history.go(10) // go ten entries forward.
+	 * ```
+	 */
+	go: (steps: number) => void;
+	/**
+	 * Checks whether or not the history is currently processing a transition.
+	 *
+	 * @returns True, if there is a transition currently in progress.
+	 */
+	isInTransition: () => boolean;
 
-		this._blocker = this._options.createBlocker(
-			this._options.getUserConfirmation
-		);
+	_historyAdapter: HistoryAdapter;
+}
 
-		this._historyAdapter = createAdapter(
-			this._adapterCallback,
-			this._options
-		);
-		this._historyAdapter.listen();
-		this.location = this._historyAdapter.getLocation();
-		this.length = this._historyAdapter.getLength();
+/**
+ * Creates a new History instance.
+ *
+ * @param createAdapter The function to create a {@link HistoryAdapter} instance.
+ * @param options The options for the History instance and the HistoryAdapter.
+ */
+export const History = (function (
+	this: History,
+	createAdapter: (
+		listener: OnAdapterLocationChange,
+		options: HistoryOptions
+	) => HistoryAdapter,
+	options?: HistoryOptions
+) {
+	let _listeners: NavigationListener[] = [];
+	let _options: HistoryOptions;
+	let _blocker: TransitionBlocker;
+	let _pendingTransition: boolean = false;
+
+	_options = options || {};
+	if (!_options.basename) {
+		_options.basename = "";
+	}
+	if (!_options.keyLength) {
+		_options.keyLength = 6;
+	}
+	if (!_options.getUserConfirmation) {
+		_options.getUserConfirmation = defaultUserConfirmation;
+	}
+	if (!_options.createBlocker) {
+		_options.createBlocker = LegacyBlocker;
 	}
 
-	private _adapterCallback = (newLocation: Location) => {
+	_blocker = _options.createBlocker(_options.getUserConfirmation);
+
+	this.length = 0;
+	this.action = ACTION.POP;
+
+	this._alertListeners = (action: string) => {
+		_listeners.forEach((listener) => listener(this.location, action));
+	};
+	this._transition = (
+		target: Location,
+		action: string,
+		onSuccess: Function,
+		onFailure?: Function
+	) => {
+		_pendingTransition = true;
+		let isBlocked = _blocker.isBlocked(target, action);
+		if (isBlocked) {
+			onFailure && onFailure();
+			return;
+		} else {
+			onSuccess();
+		}
+		_pendingTransition = false;
+		this.action = action;
+		this.length = this._historyAdapter.getLength();
+	};
+
+	this._adapterCallback = (newLocation: Location) => {
 		const oldLocation = this.location;
 		this._transition(
 			newLocation,
@@ -100,80 +210,35 @@ export class History {
 		);
 	};
 
-	private _alertListeners = (action: string) => {
-		this._listeners.forEach((listener) => listener(this.location, action));
-	};
-
-	private _transition = (
-		target: Location,
-		action: string,
-		onSuccess: Function,
-		onFailure?: Function
-	) => {
-		this._pendingTransition = true;
-		let isBlocked = this._blocker.isBlocked(target, action);
-		if (isBlocked) {
-			onFailure && onFailure();
-			return;
-		} else {
-			onSuccess();
-		}
-		this._pendingTransition = false;
-		this.action = action;
-		this.length = this._historyAdapter.getLength();
-	};
-
-	/**
-	 * Register a listener for all changes in location.
-	 * @param listener The callback to register. Will be called with {@link Location} and action.
-	 * @returns A callback to unregister the newly added listener.
-	 */
-	listen = (listener: NavigationListener): (() => void) => {
-		this._listeners.push(listener);
+	this.listen = (listener: NavigationListener): (() => void) => {
+		_listeners.push(listener);
 		return () => {
 			this.unlisten(listener);
 		};
 	};
 
-	/**
-	 * Removes / disables a listener.
-	 *
-	 * @param listener The listener to disable.
-	 */
-	unlisten = (listener: NavigationListener): void => {
-		let index = this._listeners.indexOf(listener);
+	this.unlisten = (listener: NavigationListener): void => {
+		let index = _listeners.indexOf(listener);
 		if (index >= 0) {
-			this._listeners.splice(index, 1);
+			_listeners.splice(index, 1);
 		}
 	};
 
-	/**
-	 * Creates an appropriate href target string for a given location.
-	 * @param target
-	 */
-	createHref = (target: Location): string => {
+	this.createHref = (target: Location): string => {
 		return this._historyAdapter.modifyPath(createPath(target));
 	};
 
-	/**
-	 * Navigate to a location with the specified method.
-	 *
-	 * @param target Target location.
-	 * @param state Desired state for the location
-	 * @param method Specify the method to use for navigation. Either "PUSH" or "REPLACE".
-	 * Default: PUSH.
-	 */
-	navigate = (
+	this.navigate = (
 		target: string | Location,
 		state: object | null,
-		method: string = "PUSH"
+		method: string = ACTION.PUSH
 	) => {
 		let newLocation = createLocation(
 			target,
 			state,
-			createKey(this._options.keyLength),
+			createKey(_options.keyLength),
 			this.location,
-			this._options.preserveSearch
+			_options.preserveSearch
 		);
 		let newPath = createPath(newLocation);
 		this._transition(newLocation, method, () => {
@@ -185,81 +250,56 @@ export class History {
 		});
 	};
 
-	/**
-	 * Push a new location to the history stack and navigate to it.
-	 *
-	 * @param target Location to go to. Either a pathname or a complete location object.
-	 * @param state Optional state to push with the location.
-	 */
-	push = (target: string | Location, state: object | null = null): void => {
+	this.push = (
+		target: string | Location,
+		state: object | null = null
+	): void => {
 		this.navigate(target, state, ACTION.PUSH);
 	};
 
-	/**
-	 * Replace the current location and state on the stack.
-	 *
-	 * @param target Location or pathname to navigate to.
-	 * @param state Desired state
-	 */
-	replace = (
+	this.replace = (
 		target: string | Location,
 		state: object | null = null
 	): void => {
 		this.navigate(target, state, ACTION.REPLACE);
 	};
 
-	/**
-	 * Configure a transition block. {@link LegacyBlocker}
-	 *
-	 * @param args - Arguments passed to {@link LegacyBlocker.block}
-	 * @returns A callback to remove the block.
-	 */
-	block = (...args: any[]): (() => void) => {
-		return this._blocker.block.apply(this._blocker, args);
+	this.block = (...args: any[]): (() => void) => {
+		return _blocker.block.apply(_blocker, args);
 	};
 
-	/**
-	 * Remove the currently configured blocker.
-	 */
-	unblock = (): void => {
-		this._blocker.unblock();
+	this.unblock = (): void => {
+		_blocker.unblock();
 	};
 
-	/**
-	 * Shorthand for go(1). Go forward in history by one entry.
-	 */
-	goBack = (): void => {
+	this.goBack = (): void => {
 		this.go(-1);
 	};
 
-	/**
-	 * Shorthand for go(-1). Go forward in history by one entry.
-	 */
-	goForward = (): void => {
+	this.goForward = (): void => {
 		this.go(1);
 	};
 
-	/**
-	 * Goes forwards or backwards in history. If the value is out of bounds, it will go as far as it can (i.E. to the first or last entry).
-	 *
-	 * @param steps Number of steps to go forward / backward.
-	 * @example
-	 * ```js
-	 * history.go(-10); // go ten entries back.
-	 *
-	 * history.go(10) // go ten entries forward.
-	 * ```
-	 */
-	go = (steps: number): void => {
+	this.go = (steps: number): void => {
 		this._historyAdapter.go(steps);
 	};
 
-	/**
-	 * Checks whether or not the history is currently processing a transition.
-	 *
-	 * @returns True, if there is a transition currently in progress.
-	 */
-	isInTransition = (): boolean => {
-		return this._pendingTransition;
+	this.isInTransition = (): boolean => {
+		return _pendingTransition;
 	};
-}
+
+	// this._adapterCallback = this._adapterCallback.bind(this);
+	this._historyAdapter = createAdapter(this._adapterCallback, _options);
+	this._historyAdapter.listen();
+	this.location = this._historyAdapter.getLocation();
+	this.length = this._historyAdapter.getLength();
+	return this;
+} as any) as {
+	new (
+		createAdapter: (
+			listner: OnAdapterLocationChange,
+			options: HistoryOptions
+		) => HistoryAdapter,
+		options?: HistoryOptions
+	): History;
+};
